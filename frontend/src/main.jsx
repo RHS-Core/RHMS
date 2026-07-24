15773c83bbbd9a90bc86c2da87178509262cd591
Nghiêm Thị Mai Diễm <24107772@st.phenikaa-uni.edu.vn>
2026-07-25 00:40:49 +0700

    fix: resolve conflict in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -11,15 +10,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
         <App />
     </BrowserRouter>
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> eeb232525fbc1c020ab8911fddfa45ce40b32bf2
 );
